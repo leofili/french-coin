01ff8d9fa79a9cb046ae42d77652bcea222e2246
Thierry Tranchant <thierry.tcht@gmail.com>
2021-05-25 12:41:37 +0200

    loan-name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_24_162710) do
+ActiveRecord::Schema.define(version: 2021_05_25_103434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2021_05_24_162710) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
     t.index ["user_id"], name: "index_loans_on_user_id"
   end
 
