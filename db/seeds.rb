d029df65a368f75703c742ac239a90bb19af260e
Thierry Tranchant <thierry.tcht@gmail.com>
2021-05-27 14:29:36 +0200

    added unvalidated loans to the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,16 @@ helen = User.find_by(email: "helen@example.com")
 #Creating fake loan
 Loan.create(user: admin)
 
+#Creating two non validated loans
+first_unvalidated_loan = Loan.create(name: 'fake1', user: helen, collateral_cents: 500000, collateral_currency: 'EUR', amount_cents: 1000000, amount_currency: 'EUR', start_date: Date.today, duration: 6, end_date: Date.today + 6.months, interest_rate: 0.0675, interest_cents: 67500)
+
+second_unvalidated_loan = Loan.create(name: 'fake2', user: helen, collateral_cents: 500000, collateral_currency: 'ETH', amount_cents: 1000000, amount_currency: 'ETH', start_date: Date.today, duration: 6, end_date: Date.today + 6.months, interest_rate: 0.0675, interest_cents: 67500)
+
+
 # Creating a validated loan in euros
 first_loan = Loan.create(name: 'fake1', user: helen, collateral_cents: 500000, collateral_currency: 'EUR', amount_cents: 1000000, amount_currency: 'EUR', start_date: Date.today, duration: 6, end_date: Date.today + 6.months, interest_rate: 0.0675, interest_cents: 67500)
+
+Transfer.create(user: helen, loan: first_loan, amount_cents: 500000, amount_currency: 'EUR', category: 'collateral_payment')
 
 first_loan.duration.times do |num|
   Payment.create(amount_cents: 177917, interest_amount_cents: 11250, refund_amount_cents: 166667, loan: first_loan, due_date: first_loan.start_date + (num + 1).months)
@@ -17,12 +25,16 @@ end
 # Creating a validated loan in ethereum
 second_loan = Loan.create(name: 'fake2', user: helen, collateral_cents: 500000, collateral_currency: 'ETH', amount_cents: 1000000, amount_currency: 'ETH', start_date: Date.today, duration: 6, end_date: Date.today + 6.months, interest_rate: 0.0675, interest_cents: 67500)
 
+Transfer.create(user: helen, loan: second_loan, amount_cents: 500000, amount_currency: 'ETH', category: 'collateral_payment')
+
 second_loan.duration.times do |num|
   Payment.create(amount_cents: 177917, interest_amount_cents: 11250, refund_amount_cents: 166667, loan: second_loan, due_date: second_loan.start_date + (num + 1).months)
 end
 
 # Creating a validated loan in euros with three already done payments
 third_loan = Loan.create(name: 'fake3', user: helen, collateral_cents: 500000, collateral_currency: 'EUR', amount_cents: 1000000, amount_currency: 'EUR', start_date: Date.today - 3.months, duration: 6, end_date: Date.today + 3.months, interest_rate: 0.0675, interest_cents: 67500)
+
+Transfer.create(user: helen, loan: third_loan, amount_cents: 500000, amount_currency: 'EUR', category: 'collateral_payment')
 
 third_loan.duration.times do |num|
   Payment.create(amount_cents: 177917, interest_amount_cents: 11250, refund_amount_cents: 166667, loan: third_loan, due_date: third_loan.start_date + (num + 1).months)
@@ -37,6 +49,8 @@ end
 
 # Creating a validated loan in ethereum with three already done payments
 fourth_loan = Loan.create(name: 'fake4', user: helen, collateral_cents: 500000, collateral_currency: 'ETH', amount_cents: 1000000, amount_currency: 'ETH', start_date: Date.today - 3.months, duration: 6, end_date: Date.today + 3.months, interest_rate: 0.0675, interest_cents: 67500)
+
+Transfer.create(user: helen, loan: fourth_loan, amount_cents: 500000, amount_currency: 'ETH', category: 'collateral_payment')
 
 fourth_loan.duration.times do |num|
   Payment.create(amount_cents: 177917, interest_amount_cents: 11250, refund_amount_cents: 166667, loan: fourth_loan, due_date: fourth_loan.start_date + (num + 1).months)
