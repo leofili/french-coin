dc43b41256bd9722abbe0564101f3815c04bfec9
leo filipetti <leo.filipetti@gmail.com>
2021-06-02 17:00:39 +0200

    loan show / homepage css

--- app/models/conversion.rb
+++ app/models/conversion.rb
@@ -1,3 +1,6 @@
+require 'json'
+require 'open-uri'
+
 class Conversion < ApplicationRecord
   def self.fill_conversion
     url = 'https://min-api.cryptocompare.com/data/v2/histoday?fsym=ETH&tsym=EUR&limit=120&api_key=e0c8e19de543fd2593c72f16715c49fbab97d0581e47c2470fbffdba91964461'
@@ -5,7 +8,7 @@ class Conversion < ApplicationRecord
     data = JSON.parse(data_serialized)
     data['Data']['Data'].each do |hash|
       if Rails.env == "development"
-        unixtime = hash['time'] <= 1_616_889_600 ? hash['time'] : hash['time']
+        unixtime = hash['time'] <= 1_616_889_600 ? hash['time'] + 3600 : hash['time']
       else
         unixtime = hash['time']
       end
