d177067bea21e63587c04f0ef6e52a901f3dae35
Danny Cardoso <dcardoso066@gmail.com>
2021-06-02 13:45:29 +0200

    edit done

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -62,10 +62,28 @@ class LoansController < ApplicationController
 
   def update
     @loan = Loan.find(params[:id])
-    authorize @loan
     @loan.update(loan_params)
+    @loan.user = current_user   
+    @loan.duration =  params[:month].to_i
+    # Envoi des données amount en centimes d'euros
+    if params[:loan][:amount_currency] == "Ethereums"
+      @loan.amount_currency = "ETH"
+      @loan.amount_cents = params[:loan][:amount_cents].to_f * 2273 * 100
+    elsif params[:loan][:amount_currency] == "Euros"
+      @loan.amount_currency = "EUR"
+      @loan.amount_cents = params[:loan][:amount_cents].to_f * 100
+    end
+    # Envoi des données collateral en centimes d'euros
+    if params[:loan][:collateral_currency] == "Ethereums"
+      @loan.collateral_currency = "ETH"
+      @loan.collateral_cents = params[:loan][:collateral_cents].to_f * 2273 * 100
+    elsif params[:loan][:collateral_currency] == "Euros"
+      @loan.collateral_currency = "EUR"
+      @loan.collateral_cents = params[:loan][:collateral_cents].to_f * 100
+    end
+    authorize @loan
     if @loan.save!
-      redirect_to loan_path
+      redirect_to loan_path(@loan)
     else
       render :edit
     end
--- app/javascript/plugins/init_conversion.js
+++ app/javascript/plugins/init_conversion.js
@@ -25,6 +25,32 @@ const initConversion = () => {
       });
     })
   };
+  if (document.getElementById("edit-loan")) {
+    const dropdowns = document.querySelectorAll(".dropboxnewloan1 input[type='radio']");
+    dropdowns.forEach((dropdown) => {
+      dropdown.addEventListener("change", (event) => {
+        event.preventDefault();
+        const dropdownType = event.currentTarget.id.split("_currency")[0];
+        const inputResult = document.querySelector(`#${dropdownType}_cents`);
+
+        let amount = inputResult.value;
+        let currency = event.currentTarget.value;
+        if (currency === "Euros") {
+          inputResult.value = (amount * 2273).toFixed(2);
+          if (inputResult === document.querySelector(`.loan_collateral_cents input`)) {
+          document.querySelector(`.collateral_span_new_loan span`).innerText  = amount * 2273;
+          }
+        };
+
+        if (currency === "Ethereums") {
+          inputResult.value = amount / 2273;
+          if (inputResult === document.querySelector(`.loan_collateral_cents input`)) {
+            document.querySelector(`.collateral_span_new_loan span`).innerText = amount / 2273;
+          }
+        };
+      });
+    })
+  };
 };
 
 export { initConversion }
