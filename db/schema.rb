69ec821871d5830e126117a1152fa2e2567b66fe
Elina Muradova <muradovaelina@gmail.com>
2021-06-03 09:28:12 +0200

    pop-up page

--- db/schema.rb
+++ db/schema.rb
@@ -103,8 +103,8 @@ ActiveRecord::Schema.define(version: 2021_06_02_090235) do
     t.string "phone_number"
     t.string "iban"
     t.string "bic"
-    t.integer "crypto_balance", default: O
-    t.integer "euro_balance", default: 0
+    t.integer "crypto_balance"
+    t.integer "euro_balance"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
